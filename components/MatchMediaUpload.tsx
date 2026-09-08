6cf82be219137f0ba9175ad3bd1c6849db8a1faf
pietromarchioro2001 <pietromarchioro2001@gmail.com>
2026-09-08 13:47:14 +0000

    e

--- components/MatchMediaUpload.tsx
+++ components/MatchMediaUpload.tsx
@@ -225,7 +225,7 @@ export default function MatchMediaGallery({ matchId, isStaffMode = false }: Matc
       {/* Modal fullscreen */}
       {selectedPhoto && !isStaffMode && (
         <div
-          className="fixed inset-0 bg-black/95 z-[100] flex items-center justify-center p-4"
+          className="fixed inset-0 bg-black/95 z-[110] flex items-center justify-center p-4"
           onClick={() => setSelectedPhoto(null)}
         >
           <button
